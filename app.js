0142b8807c9d801670613b5e86949e8b4db436dc
Berkay GURSOY <berkaygursoy@gmail.com>
2018-12-07 03:15:37 +0300

    remove connect-browsersync

--- app.js
+++ app.js
@@ -5,20 +5,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var aboutRouter = require('./routes/about');
 
 var app = express();
-
-// BrowserSync at development host
-if (app.get('env') === 'development') {
-    var browserSync = require('browser-sync');
-    var bs = browserSync.create().init({ logSnippet: false });
-    app.use(require('connect-browser-sync')(bs));
-}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
